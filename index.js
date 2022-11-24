9136015015fb50e9c042427df1afb9684e59e492
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-24 10:50:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// code your solution here
+// // code your solution here
+
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun(`bathe my dog`);
+
+function mondayWork (activity = `go to the office`){
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork();
+
+
+function wrapAdjective(highlight = '*'){
+    return function(adj = "a hard worker"){
+        return `You are ${highlight}${adj}${highlight}!`;
+    }
+}
+wrapAdjective();
+
+
+
+
+
+
+// function saturdayFun(){
+    
+// }
+// function saturdayFun(does ="roller-skate"){
+//     return("This Saturday, I want to roller-skate!");
+// }
+// // function saturdayFun(){
+// //    return ("This Saturday, I want to bathe my dog!");
+
+// saturdayFun(`bathe my dog`);
+
+// function mondayWork (activity = `go to the office`){
+//     return `This Monday, I will ${activity}.`;
+// }
+
+// mondayWork();
+
+
+// function wrapAdjective(highlight = '*'){
+//     return function(adj = "a hard worker"){
+//         return `You are ${highlight}${adj}${highlight}!`;
+//     }
+// }
+// wrapAdjective();
+
+
+
